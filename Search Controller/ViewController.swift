e5018fd622b27a80913e40b52a27c4050e176cbb
Asad Ardiansyah <asad.ardiansyah@gmail.com>
2020-10-14 21:40:44 +0700

    initial commit

---
+++ Search Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Search Controller
+//
+//  Created by Asad Ardiansyah on 14/10/20.
+//  Copyright © 2020 Asad Ardiansyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
